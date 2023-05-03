0cdb1b3efda08e2a7458f6cd0b0be9dcce8c05f4
Mariana Caldas <developer.mariana@outlook.com>
2023-05-03 13:10:20 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal do Organo" />
+        </header>
+    )
+
+}
+
+export default Banner
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,23 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...` 
+
+    //  let valor = 'Mariana Caldas'
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
