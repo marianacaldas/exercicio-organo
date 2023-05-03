3357d554bc4b89e78ccbc81ae35564a08f1b59aa
Mariana Caldas <developer.mariana@outlook.com>
2023-05-03 13:12:46 -0300

    Update index.js

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -26,7 +26,7 @@ const Rodape = () => {
         </section>
         <section>
             <p>
-                Desenvolvido por Alura.
+                Desenvolvido por Mariana.
             </p>
         </section>
     </footer>)
